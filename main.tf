270ef0c4b2a988e8003343faac4930c846f58d65
Arm Bun <bellotaophyc@outlook.com>
2022-08-19 02:51:13 +0000

    create a security group to allow traffic on port 8080

--- main.tf
+++ main.tf
@@ -12,12 +12,29 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_instance" "cap_server" {
+resource "aws_instance" "cap-server" {
   ami           = "ami-005e54dee72cc1d00" # us-west-2
   instance_type = "t2.micro"
+
+  user_data = <<-EOF
+                #!/usr/bin/env bash
+                echo "Hello, World" > index.html
+                nonhup busybox httpd -f -p 8080 &
+                EOF
 
   tags = {
     Name = "cap_on"
   }
 
+}
+
+resource "aws_security_group" "instance" {
+  name = "cap-server-group"
+
+  ingress {
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
 }
