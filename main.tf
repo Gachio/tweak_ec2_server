c1d45a54e4664180cac5091d4d0fa313c53e0324
Arm Bun <bellotaophyc@outlook.com>
2022-08-19 03:42:07 +0000

    set the serverPort for userdata script

--- main.tf
+++ main.tf
@@ -16,8 +16,8 @@ resource "aws_security_group" "capsg" {
   name = "cap-server-group"
 
   ingress {
-    from_port        = 8080
-    to_port          = 8080
+    from_port        = var.server_port
+    to_port          = var.server_port
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
@@ -31,7 +31,7 @@ resource "aws_instance" "cap-server" {
   user_data = <<-EOF
                 #!/usr/bin/env bash
                 echo "Hello, World" > index.html
-                nonhup busybox httpd -f -p 8080 &
+                nonhup busybox httpd -f -p ${var.server_port} &
                 EOF
 
   tags = {
